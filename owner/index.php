3a97cbbd0f16ebfe7b16a748de8f4fbe933bb169
zukardex <zenzenram@gmail.com>
2023-03-11 04:30:31 +0530

    updated index.php for owner

--- owner/index.php
+++ owner/index.php
@@ -3,6 +3,22 @@ session_start();
 if(!(isset($_SESSION['owner']))){
     header("Location: register.php");
 }
+function getRequests(){
+    return "No pending requests";
+}
+
+if(isset($_POST['token'])){
+    require "../config.php";
+    $sql = "SELECT name FROM `token_customr` where token ='". $_POST['token'] . "';";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        //Token was found on the database
+        while($rowt= ( $result->fetch_assoc())){
+        echo"found " . $rowt['name'];
+        }
+    }
+      $conn->close();
+}
 ?>
 <style>
 div{
@@ -18,8 +34,14 @@ div{
 </tr>
 
 <tr>
-    <td><div id="reqsArea" width="30%" height="100%" align="left" > tyu  </div></td>
-    <td><div id="connArea" width="30%" height="100%" align="center" > yuy  </div></td>
+    <td><div id="reqsArea" width="30%" height="100%" align="left" > <?php echo getRequests(); ?>  </div></td>
+    <td>
+        <div id="connArea" width="30%" height="100%" align="center" >
+                <form method="post"><input type="token" name="token" required><br><input type="submit"></form>
+        </div>
+    </td>
+
+
     <td><div id="pushArea" width="30%" height="100%" align="right" > uy  </div></td>
 </tr>
 
